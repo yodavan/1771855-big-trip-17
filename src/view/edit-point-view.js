790112008ebab3e03059791ee63c2db282a7294e
yodavan <84133554+yodavan@users.noreply.github.com>
2022-05-25 22:24:37 +0300

    внес изменения

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -92,7 +92,7 @@ export default class EditPointView extends AbstractStatefulView {
                       <span class="visually-hidden">Price</span>
                       &euro;
                     </label>
-                    <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${ basePrice }">
+                    <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${ basePrice }">
                   </div>
 
                   <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
